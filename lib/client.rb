660ae24c2339ed129bf544f6d464250ca983dd7c
Austin <aokincaid@gmail.com>
2015-08-21 11:20:34 -0700

    Add specs and methods to allow the ability to save new instances of the custom class Clients to the database

--- lib/client.rb
+++ lib/client.rb
@@ -6,4 +6,25 @@ class Client
     @name = attributes.fetch(:name)
     @stylist_id = attributes.fetch(:stylist_id)
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{@name}', #{@stylist_id}) RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+
+  define_singleton_method(:all) do
+    results = DB.exec("SELECT * FROM clients;")
+    all_clients = []
+    results.each() do |client|
+      id = client.fetch('id').to_i()
+      name = client.fetch('name')
+      stylist_id = client.fetch('stylist_id').to_i()
+      all_clients.push(Client.new({:id => id, :name => name, :stylist_id => stylist_id}))
+    end
+    all_clients
+  end
+
+  define_method(:==) do |other_client|
+    self.id().==(other_client.id()).&(self.name().==(other_client.name())).&(self.stylist_id().==(other_client.stylist_id()))
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,33 +8,33 @@ describe(Client) do
     end
   end
 
-  # describe('#save') do
-  #   it('saves a specified instance of the Stylist class') do
-  #     stylist = Stylist.new({:id => nil, :name => 'Austin Kincaid'})
-  #     stylist.save()
-  #     expect(Stylist.all()).to(eq([stylist]))
-  #   end
-  # end
-  #
-  # describe('.all') do
-  #   it('returns an empty array if no instances have been saved to the database') do
-  #     expect(Stylist.all()).to(eq([]))
-  #   end
-  #
-  #   it('returns all instances of the Stylist class if instances are saved') do
-  #     stylist = Stylist.new({:id => nil, :name => 'Austin Kincaid'})
-  #     stylist.save()
-  #     stylist2 = Stylist.new({:id => nil, :name => 'Sam Harris'})
-  #     stylist2.save()
-  #     expect(Stylist.all()).to(eq([stylist, stylist2]))
-  #   end
-  # end
-  #
-  # describe('#==') do
-  #   it('equates instances of the custom class Stylists in such a way that Rspec can handle') do
-  #     stylist = Stylist.new({:id => nil, :name => 'Austin Kincaid'})
-  #     stylist.save()
-  #     expect(Stylist.all()).to(eq([stylist]))
-  #   end
-  # end
+  describe('#save') do
+    it('saves a specified instance of the Client class') do
+      client = Client.new({:id => nil, :name => 'Austin Kincaid', :stylist_id => 1})
+      client.save()
+      expect(Client.all()).to(eq([client]))
+    end
+  end
+
+  describe('.all') do
+    it('returns an empty array if no instances have been saved to the database') do
+      expect(Client.all()).to(eq([]))
+    end
+
+    it('returns all instances of the Client class if instances are saved') do
+      client = Client.new({:id => nil, :name => 'Austin Kincaid', :stylist_id => 1})
+      client.save()
+      client2 = Client.new({:id => nil, :name => 'Sam Harris', :stylist_id => 2})
+      client2.save()
+      expect(Client.all()).to(eq([client, client2]))
+    end
+  end
+
+  describe('#==') do
+    it('equates instances of the custom class Clients in such a way that Rspec can handle') do
+      client = Client.new({:id => nil, :name => 'Austin Kincaid', :stylist_id => 1})
+      client.save()
+      expect(Client.all()).to(eq([client]))
+    end
+  end
 end
