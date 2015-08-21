83723fd69bf2376d7980240038c3db8cf452cff1
Austin <aokincaid@gmail.com>
2015-08-21 13:25:56 -0700

    Add app.rb file with basic initial routing to homepage

---
+++ app.rb
@@ -0,0 +1,12 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/stylist")
+require("./lib/client")
+require('pg')
+
+DB = PG.connect({:dbname => "hair_salon_test"})
+
+get("/") do
+  erb(:index)
+end
